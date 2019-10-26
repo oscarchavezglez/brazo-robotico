0cdf8e51696ad45f4147588c4901bab2bf3d318b
oscarchavezglez <44283200+oscarchavezglez@users.noreply.github.com>
2019-10-25 23:37:47 -0500

    Add files via upload

--- proyecto.asm
+++ proyecto.asm
@@ -7,18 +7,24 @@ CONFIG MCLRE = ON 	;Reset encendido
 CONFIG DEBUG = OFF	; Disables Debug mode    
 CONFIG LVP = OFF	; Disables Low-Voltage programming    
 CONFIG FOSC = INTOSCIO	; Enables internal oscillator
-CONFIG nPWRTEN = ON	;PWR UP Timer habilitado
+CONFIG nPWRTEN = Off	;PWR UP Timer habilitado
 ;**************************************************************************************
-org 000
-    
+org 000  
+ XB EQU 0X00   
+     
+ MainLoop:
+MOVLB  0xF0 
+CLRF ANSELB
+MOVLW  0x0F 
+MOVWF TRISB 
+BCF PORTB,0
+CLRF TRISC ;Se configura el puerto C como salidas digitales con Trisc
 ;PWM periodo = (PR2 + 1)*4*Tosc*Prescaler
 ;PR2 = (PWM periodo/(4*Tosc*Prescaler))-1
 ;PR2 = (1/50Hz/(4*(1/500KHz)*16))-1    
-;PR2 =155 --> Hex=9B    
+;PR2 =155 --> Hex=9B      
 MOVLW  0x9B   
-MOVWF PR2         
-MOVLB  0xF0     
-CLRF TRISC ;Se configura el puerto C como salidas digitales con Trisc  
+MOVWF PR2      
 ;PWM Duty Cycle = (CCPRXL:CCPXCON<5:4>)*TOSC *(TMR2 Prescale Value)
 ;CCPRXL=PWM/(TOSC*(TMR2 Prescale Value))
 ;CCPRXL=((1/50Hz)/(1/500KHz)*16)
@@ -29,23 +35,28 @@ CLRF TRISC ;Se configura el puerto C como salidas digitales con Trisc
 ;X --->  -90°=2.5% <---> 0°=7.5% <--->  90°=12.5%
 ;625--->100%
 ;Y=?<---X   
-;Y --->  -90°= <---> 0°=7.5% <--->  90°=12.5%    
-    ;00000100
-MOVLW b'00000100' ;-90°= 00100111 0°=11001011 90°=10010011
+;Y --->  -90°=16 <---> 0°=47 <--->  90°=78    
+;Y --->  -90°=00010000 <---> 0°=00101111 <--->  90°=01001110   
+;Y --->  -90°=00000100 <---> 0°=11001011 <--->  90°=10010011    
+MOVLW 0x10
+MOVWF XB
+INC       
+BTFSC PORTB,0 ;  PORTB,0 "Bit 0" del puerto A esta en 0 lógico (0volts)?
+INCF XB,f
+MOVF XB,W
 MOVWF CCPR1L 
-        
-   
-
-    
-
-
-    
-MOVLW b'00000101' ;  00000111 original     ;0000 0011 ; 00000101
+MOVLW b'00000111' ;0000011 ;00000101 ;1:1 Postscaler ,Timer2 is on,Prescaler is 16 tablade registro
 MOVWF T2CON
-MOVLW b'00001101' ;00000000 ;00001101 ORIGINAL
+MOVLW b'00001100'; 00001100  ;CCPxCON: STANDARD CCPx CONTROL REGISTER 00001101
 MOVWF CCP1CON    
 MOVLW 0x00
-MOVWF TMR2
+MOVWF TMR2    
+GOTO INC
+ 
+
+ 
+ 
+ 
 END
     
     
